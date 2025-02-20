14b8aa2f613a46bf3c78068c934a6ae1c6bbd0d4
pantharshit007 <aaryanpandit3527@gmail.com>
2025-02-20 17:49:14 +0530

    ✨ Feat: update README with project details, installation instructions, and tech stack; remove buggy layout component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,23 +4,28 @@ import CTA from "@/components/Landing/CTA";
 import Footer from "@/components/Landing/Footer";
 import Pricing from "@/components/Landing/Pricing";
 import FAQ from "@/components/Landing/Faq";
+import MouseMoveEffect from "@/hooks/use-mouse-effect";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
-      {/* Background gradients */}
-      <div className="h-full w-full bg-slate-950">
-        <div className="absolute bottom-0 left-0 right-0 top-[3.5rem] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-      </div>
+    <>
+      <MouseMoveEffect />
 
-      <div className="relative">
-        <Hero />
-        <Features />
-        {/* <Pricing /> */}
-        <FAQ />
-        <CTA />
-        <Footer />
+      <div className="relative min-h-screen">
+        {/* Background gradients */}
+        <div className="h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-0 right-0 top-[3.5rem] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+        </div>
+
+        <div className="relative">
+          <Hero />
+          <Features />
+          {/* <Pricing /> */}
+          <FAQ />
+          <CTA />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
